dda33b036f182813a1badb1b6743969102622038
Miller <guimigc@protonmail.com>
2025-02-04 11:36:56 -0300

    docs: implementando readme e comentando código

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # EmbarcaTech_Semaforo
-Utilizando temporizador para simular um sistema de semáforo simples
+Este projeto consiste na utilização de um temporizador periódico para a simulação de um sistema de semáforo.
+
+## Estrutura do Código
+O código está organizado da seguinte maneira:
+
+- `inicializar_pinos()`: Inicializa os pinos a serem utilizados.
+- `ligar_vermelho()`: Acende o LED vermelho.
+- `ligar_amarelo()`: Acende o LED amarelo.
+- `ligar_verde()`: Acende o LED verde.
+- `repeating_timer_callback(struct repeating_timer *t)`: Executa a lógica do temporizador no tempo estipulado.
+- `add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer)`: Configura o temporizador para chamar a função `repeating_timer_callback` no tempo estipulado.
+
+## Desenvolvedor
+Guilherme Miller Gama Cardoso
--- Semaforo.c
+++ Semaforo.c
@@ -2,13 +2,15 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
+// Defines dos pinos
 #define red_pin 11
 #define yellow_pin 12
 #define green_pin 13
 
+// Variáveis globais
 uint8_t contador = 0;
 
-
+// Inicializa pinos utilizados
 void inicializar_pinos(){
     gpio_init(red_pin);
     gpio_set_dir(red_pin, GPIO_OUT);
@@ -18,24 +20,28 @@ void inicializar_pinos(){
     gpio_set_dir(green_pin, GPIO_OUT);
 }
 
+// Liga LED vermelho
 void ligar_vermelho(){
     gpio_put(red_pin, 1);
     gpio_put(yellow_pin, 0);
     gpio_put(green_pin, 0);
 }
 
+// Liga LED amarelo
 void ligar_amarelo(){
     gpio_put(red_pin, 0);
     gpio_put(yellow_pin, 1);
     gpio_put(green_pin, 0);
 }
 
+// Liga LED verde
 void ligar_verde(){
     gpio_put(red_pin, 0);
     gpio_put(yellow_pin, 0);
     gpio_put(green_pin, 1);
 }
 
+// Função para executar lógica quando temporizador é chamado
 bool repeating_timer_callback(struct repeating_timer *t){
 
     switch (contador){
@@ -59,15 +65,15 @@ bool repeating_timer_callback(struct repeating_timer *t){
 }
 
 int main(){
-    struct repeating_timer timer;
+    struct repeating_timer timer; // Estrutura para armazenar informações do temporizador
 
     stdio_init_all();
     inicializar_pinos();
 
-    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer); // Função que configura tempo do temporizador e chama função repeating_timer_callback
 
     while (true) {
-        printf("Funcao principal rodando\n");
+        printf("Funcao principal rodando\n"); // Mensagem da rotina principal
         sleep_ms(1000);
     }
 }
